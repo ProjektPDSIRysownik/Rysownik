ebddaa2ae9d592ded42c9448396682d7aa7797b0
Parada <paradowskim94@gmail.com>
2018-05-29 17:02:58 +0200

    rejestracja - password check

--- Validate.js
+++ Validate.js
@@ -1,4 +1,4 @@
-function validate(){
+function validateLogin(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("pass").value;
 
@@ -11,5 +11,47 @@ if( username == "user" && password == "Password123"){
 else{
 	alert("login and/or password are incorrect");
 }
+
+}
+
+function validateSignUp(){
+	var signName = document.getElementById("signName");
+	var signMail = document.getElementById("signMail");
+	var signPass = document.getElementById("signPass");
+	var signPassRepeat = document.getElementById("signPassRepeat");
+
+	//todo : sprawdzic czy username i email istnieje w bazie
+	if(signPass.value != signPassRepeat.value){
+		alert("passwords are not same");
+		return 0;
+	}
+	if(passwordCheck(signPass)){
+		//todo: dodac usera do bazy
+		alert("registration succesfull");
+	}
+}
+
+function passwordCheck(password){
+	var lowerCase = /[a-z]/g;
+	var upperCase = /[A-Z]/g;
+	var numbers = /[0-9]/g;
+
+	if(!password.value.match(lowerCase)){
+		alert("password need to have at least 1 lower case letter");
+		return 0;
+	}
+	if(!password.value.match(upperCase)){
+		alert("password need to have at least 1 upper case letter");
+		return 0;
+	}
+	if(!password.value.match(numbers)){
+		alert("password need to have at least 1 number");
+		return 0;	
+	}
+	if(!(password.value.length >= 8)){
+		alert("password need to be at least 8 characters long");
+		return 0;	
+	}
+	return 1;
 
 }
