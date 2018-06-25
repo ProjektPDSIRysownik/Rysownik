87076246b0d05d92af6c595df64ff64bb894497f
MikolajBB <gruby601@gmail.com>
2018-06-26 00:36:22 +0200

    Dodano możliwosc rejestracji oraz logowania

--- js/serverQueries.js
+++ js/serverQueries.js
@@ -1,15 +1,19 @@
-function addUserToDatabase(signName, signMail, signPass) {
+var message;
+var addInfo;
+
+function addUserToDatabase(signName, signMail, signPass){
     var xmlhttp = new XMLHttpRequest();
-    var addUserStatus = "NO";
-    xmlhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        addUserStatus = this.responseText;
-        return addUserStatus;
-      }
-    };
+    xmlhttp.onreadystatechange = function(){
+
+        if(this.readyState == 4 && this.status == 200){
+			msg = this.responseText;
+            alert(this.responseText);
+        }
+    }
     xmlhttp.open("POST", "http://localhost/serverCode/addUserToDatabase.php", true);
     xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-    xmlhttp.send("login='" + signName + "'&passwd='" + signPass + "'&email='" + signMail +"'");
+    xmlhttp.send("login=" + signName + "&passwd=" + signPass + "&email=" + signMail);
+
 }
 
 function checkForLastLoggedInUser()
@@ -24,4 +28,28 @@ xmlhttp.onreadystatechange = function() {
 xmlhttp.open("GET", "http://localhost/serverCode/checkLastLoggedInUser.php", true);
 xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 xmlhttp.send(); 
+}
+
+function loginUser(signName, signPass){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function(){
+
+        if(this.readyState == 4 && this.status == 200){
+            var myArr = JSON.parse(this.responseText);
+			message = myArr.msg;
+			addInfo = myArr.add_info;
+			alert(message);
+        }
+    }
+    xmlhttp.open("POST", "http://localhost/serverCode/loginUser.php", true);
+    xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xmlhttp.send("login=" + signName + "&passwd=" + signPass);
+}
+
+function getMsg(){
+  return msg;
+}
+
+function getAddInfo(){
+  return addInfo;
 }
--- serverCode/addUserToDatabase.php
+++ serverCode/addUserToDatabase.php
@@ -1,30 +1,37 @@
 <?php
+header('Access-Control-Allow-Origin: *');
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "rysownik";
+$msg = "";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error) 
+if ($conn->connect_error)
 {
     die("Connection failed: " . $conn->connect_error);
-} 
+}
 
 $login = $_POST["login"];
 $passwd = $_POST["passwd"];
 $email = $_POST["email"];
 
-$sql = 'INSERT INTO `users` (`login`, `Password`, `email`) VALUES ("' . $login . '","' . $passwd . '","' . $email . '")';
-if ($conn->query($sql) === TRUE) 
-{
-    echo "OK";
-} 
-else 
-{
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
+$sql = 'SELECT * FROM `users` WHERE `login`="'.$login.'"';
+$result = $conn->query($sql);
+    if ($result->num_rows <= 0) {
+        $output = array();
 
+        $passwd = hash('sha1', $passwd);
+
+        $sql = 'INSERT INTO `users` (`login`, `Password`, `email`) VALUES ("' . $login . '","' . $passwd . '","' . $email . '")';
+        if ($conn->query($sql) === TRUE) {
+           echo "OK";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    }else{
+        echo "Taki login już istnieje";
+    }
 
 $conn->close();
-
 ?>
