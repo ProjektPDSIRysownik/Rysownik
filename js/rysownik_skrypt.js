f353ccbdaf32df6d8aeffe2a536ce3fdf5dbb1d9
MikolajBB <gruby601@gmail.com>
2018-06-26 09:45:35 +0200

    Dodano mozliwosc zapisu obrazu do bazy

--- js/rysownik_skrypt.js
+++ js/rysownik_skrypt.js
@@ -88,7 +88,10 @@ var canvas, ctx, flag = false,
 		dlLink.download = fileName;
 		dlLink.href = imgURL;
 		dlLink.dataset.downloadurl = [MIME_TYPE, dlLink.download, dlLink.href].join(':');
-
+		
+		login = getLogin();
+		addImageToDatabase(login, fileName);
+		
 		document.body.appendChild(dlLink);
 		dlLink.click();
 		document.body.removeChild(dlLink);
--- js/serverQueries.js
+++ js/serverQueries.js
@@ -1,12 +1,12 @@
 var message;
 var addInfo;
+var login = "kupka";
 
 function addUserToDatabase(signName, signMail, signPass){
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function(){
 
         if(this.readyState == 4 && this.status == 200){
-			msg = this.responseText;
             alert(this.responseText);
         }
     }
@@ -36,6 +36,7 @@ function loginUser(signName, signPass){
 
         if(this.readyState == 4 && this.status == 200){
             var myArr = JSON.parse(this.responseText);
+			login = signName;
 			message = myArr.msg;
 			addInfo = myArr.add_info;
 			alert(message);
@@ -46,10 +47,27 @@ function loginUser(signName, signPass){
     xmlhttp.send("login=" + signName + "&passwd=" + signPass);
 }
 
+function addImageToDatabase(login, fileName){
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function(){
+
+        if(this.readyState == 4 && this.status == 200){
+			alert(this.responseText);
+        }
+    }
+    xmlhttp.open("POST", "http://localhost/serverCode/addImageToDatabase.php", true);
+    xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xmlhttp.send("login=" + login + "&image=" + fileName);
+}
+
 function getMsg(){
   return msg;
 }
 
 function getAddInfo(){
   return addInfo;
+}
+
+function getLogin(){
+  return login;
 }
